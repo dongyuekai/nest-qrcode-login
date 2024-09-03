fc8f9c4ac9d0e8e6a5c5cf3600cf7db0fc45f5fe
dongyuekai <457545648@qq.com>
2024-09-03 11:10:17 +0800

    feat: 完善代码

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { randomUUID } from 'crypto';
 import * as qrcode from 'qrcode';
 
+interface QrCodeInfo {
+  status:
+    | 'noscan'
+    | 'scan-wait-confirm'
+    | 'scan-confirm'
+    | 'scan-cancel'
+    | 'expired';
+  userInfo?: {
+    userId: number;
+  };
+}
+const map = new Map<string, QrCodeInfo>();
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -16,10 +28,47 @@ export class AppController {
   async generate() {
     // 用node的crypto模块生成一个随机的uuid
     const uuid = randomUUID();
-    const dataUrl = await qrcode.toDataURL(uuid);
+    map.set(`qrcode_${uuid}`, {
+      status: 'noscan',
+    });
+    // 用 qrcode 生成二维码，只不过转成 base64 返回
+    const dataUrl = await qrcode.toDataURL(
+      `http://172.16.1.144:3000/pages/confirm.html?id=${uuid}`,
+    );
     return {
       qrcode_id: uuid,
       img: dataUrl,
     };
+  }
+  @Get('qrcode/check')
+  async check(@Query('id') id: string) {
+    return map.get(`qrcode_${id}`);
+  }
+  @Get('qrcode/scan')
+  async scan(@Query('id') id: string) {
+    const info = map.get(`qrcode_${id}`);
+    if (!info) {
+      throw new BadRequestException('二维码已过期');
+    }
+    info.status = 'scan-wait-confirm';
+    return 'success';
+  }
+  @Get('qrcode/confirm')
+  async confirm(@Query('id') id: string) {
+    const info = map.get(`qrcode_${id}`);
+    if (!info) {
+      throw new BadRequestException('二维码已过期');
+    }
+    info.status = 'scan-confirm';
+    return 'success';
+  }
+  @Get('qrcode/cancel')
+  async cancel(@Query('id') id: string) {
+    const info = map.get(`qrcode_${id}`);
+    if (!info) {
+      throw new BadRequestException('二维码已过期');
+    }
+    info.status = 'scan-cancel';
+    return 'success';
   }
 }
